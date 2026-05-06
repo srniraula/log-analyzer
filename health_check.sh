e1eee4ba43fe4ccd36a164c6abf9e98e3d80f1ee
srniraula <srniraula12@gmail.com>
2026-05-06 11:56:25 +0545

    Added these health checks: 1. CPU usage 2. Memory usage 3. Number of running processes 4. System uptime 5. Load average 6. Network Connectivity 7. Top 3 highest CPU consuming processes 8. TOP 3 highest memory consuming processes 9. Disk Usage in percentage

--- health_check.sh
+++ health_check.sh
@@ -2,25 +2,65 @@
 LOGFILE="/home/srn/health.log"
 TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
-# Checking Docker status
-echo "##########################"
-echo "Docker is: "
-docker_status=`systemctl is-active docker`
-echo $docker_status
-echo "Docker is $docker_status in the system" >> $LOGFILE
-echo "##########################"
+{
+#CPU USAGE
+cpu_usage=$(top -bn1 | grep '%Cpu(s)' | awk '{print $2}')
+if (( $(echo "$cpu_usage <= 70" | bc -l) )); then
+	echo "[INFO]:	[$TIMESTAMP]	CPU: $cpu_usage"
+elif (( $(echo "$cpu_usage > 70" | bc -l) )); then
+	echo "[WARNING]:	[$TIMESTAMP]	CPU: $cpu_usage"
+fi
+
+#MEMORY USAGE
+mem_usage=$(free -m | grep 'Mem:' | awk '{printf "%.2f", $3/$2 * 100}')
+if (( $(echo "$mem_usage <= 70" | bc -l) )); then
+	echo "[INFO]:	[$TIMESTAMP]	Mem: $mem_usage"
+elif (( $(echo "$mem_usage > 70" | bc -l) )); then
+	echo "[WARNING]:	[$TIMESTAMP]	Mem: $mem_usage"
+else
+	echo "logging error for memory usage"
+fi
+
+#Running Processes count
+echo "[INFO]:	[$TIMESTAMP]	Number of running processes: $(ps aux | wc -l)"
+
+# System uptime
+echo "[INFO]:	[$TIMESTAMP]	Uptime: $(uptime -p)"
+
+# Load Average
+read load_avg_1min load_avg_5min load_avg_15min num_rproc_to_total_proc latest_pid < /proc/loadavg
+
+if (( $(echo "$load_avg_1min/$(nproc) > 1" | bc -l) || $(echo "$load_avg_5min/$(nproc) > 1" | bc -l) || $(echo "$load_avg_15min/$(nproc) > 1" | bc -l) )); then
+	if (( $(echo "$load_avg_1min > $load_avg_5min" | bc -l) )); then
+		echo "[WARNING]:	[$TIMESTAMP]	cpu load average: $load_avg_1min, $load_avg_5min, $load_avg_15min, LOAD IS INCREASING and CPU utilization is > 100%"
+	else
+		echo "[WARNING]:	[$TIMESTAMP]	cpu load average: $load_avg_1min, $load_avg_5min, $load_avg_15min and CPU utilization is > 100%"
+	fi
+else
+	echo "[INFO]:	[$TIMESTAMP]	cpu load average: $load_avg_1min, $load_avg_5min, $load_avg_15min"
+fi
+
+# Network connectivity
+ping -c 1 8.8.8.8 > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "[INFO]:	[$TIMESTAMP]	Network connectivity: reachable"
+else
+	echo "[WARNING]:	[$TIMESTAMP]	Network connectivity: not reachable"
+fi
+
+# TOP 3 CPU consuming processes
+top3_cpu_cproc=$(ps aux --sort=-%cpu | awk 'NR>1 && NR<=4 {print $11,$3}')
+echo "[INFO]:	[$TIMESTAMP]	Top 3 CPU consuming processes: "$top3_cpu_cproc""
+
+# Top 3 MEMORY consuming processes
+top3_mem_cproc=$(ps aux --sort=-%mem | awk 'NR>1 && NR<=4 {print $11, $4}')
+echo "[INFO]:	[$TIMESTAMP]	Top 3 Memory consuming processes: "$top3_mem_cproc""
 
 # Checking disk usage
-echo "##########################"
-echo "Disk usage of main SDD is: "
 disk_usage=`df -h | grep /dev/nvme0n1p2 | awk '{gsub(/%/, "", $5); print $5}'`
 if [ $disk_usage -gt 80 ]; then 
-	echo "WARNING: The disk usage is above 80%"
-	echo "WARNING: The disk usage is above 80% at $TIMESTAMP" >> $LOGFILE
+	echo "[WARNINGING]:	[$TIMESTAMP]	Disk usage:$disk_usage (greater than 80%)"
 else
-	echo "The disk usage is $disk_usage at $TIMESTAMP"
-	echo "The disk usage is $disk_usage at $TIMESTAMP" >> $LOGFILE
+	echo "[INFO]:	[$TIMESTAMP]	Disk usage:$disk_usage (normal)"
 fi
-echo "##########################"
-
-
+} > $LOGFILE 2>&1
